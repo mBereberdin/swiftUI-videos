eae522a4a87442f9f7a886c3a00c76e9ab9510c7
Mikhail Bereberdin <bereberdin999@icloud.com>
2024-05-18 17:06:32 +0700

    Добавление основной логики приложения
    - Добавлена модель видео;
    - реализовано вью списка видео;
    - добавлены изображения для видео.

--- src/SwiftUI-Videos/ContentView.swift
+++ src/SwiftUI-Videos/ContentView.swift
@@ -8,17 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var videos: [Video] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(videos) { video in
+                HStack {
+                    
+                    Image(video.imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 90.0)
+                        .cornerRadius(8)
+                    
+                    Spacer().frame(width: 16)
+                    
+                    VStack(alignment: .leading) {
+                        
+                        Text(video.title)
+                            .fontWeight(.semibold)
+                            .lineLimit(2)
+                            .minimumScaleFactor(0.75)
+                        
+                        Spacer().frame(height: 5)
+                        
+                        Text(video.uploadDate)
+                            .font(.subheadline)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+            }.navigationTitle(Text("Sean's Videos"))
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(videos: VideoList.topTwelve)
 }

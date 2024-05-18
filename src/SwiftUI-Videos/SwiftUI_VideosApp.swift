5ab30a4b6fae3bcad37c0088053e2336da555d02
Mikhail Bereberdin <bereberdin999@icloud.com>
2024-05-18 16:17:09 +0700

    Инициализация проекта

---
+++ src/SwiftUI-Videos/SwiftUI_VideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VideosApp.swift
+//  SwiftUI-Videos
+//
+//  Created by Михаил Беребердин on 18.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
